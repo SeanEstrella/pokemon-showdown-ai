f40e12f69dc012e8f1e2cf34f3d421f3a93b2609
Sean Estrella <sean.estrella2002@gmail.com>
2024-11-21 14:24:29 -0800

    update with type annotations

--- src/bot/max_damage.py
+++ src/bot/max_damage.py
@@ -1,14 +1,15 @@
+from poke_env.environment.abstract_battle import AbstractBattle
+from poke_env.player.battle_order import BattleOrder
 from poke_env.player.player import Player
+from poke_env.ps_client.account_configuration import AccountConfiguration
 
 
 class MaxDamagePlayer(Player):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: AccountConfiguration, **kwargs: int):
         super().__init__(*args, **kwargs)
 
-    def choose_move(self, battle):
+    def choose_move(self, battle: AbstractBattle) -> BattleOrder:
         if battle.available_moves:
             best_move = max(battle.available_moves, key=lambda move: move.base_power)
             return self.create_order(best_move)
-        else:
-            return self.choose_random_move(battle)
-        
+        return self.choose_random_move(battle)
